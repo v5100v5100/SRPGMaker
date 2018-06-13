2598216ae85dd898d5e8ccdae272196a2290b921
liuweihang <liuweihang@medp.ai>
2018-06-13 19:33:10 +0800

    添加TiledSet

---
+++ Tiled/TiltedSet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tiled
+{
+    /// <summary>
+    /// Tiled设置
+    /// </summary>
+    public class TiltedSet
+    {
+
+    }
+}
